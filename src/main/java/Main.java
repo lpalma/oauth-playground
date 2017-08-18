53f69ddba908a1c3569e1c97a011e4fdaaffe12d
Luciano Palma <lucianopalma90@gmail.com>
2017-08-18 11:46:06 +0100

    retrieve user profile from callback

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,19 @@
-import com.google.gson.Gson;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
-import org.pac4j.core.profile.ProfileManager;
 import org.pac4j.oauth.client.Google2Client;
-import org.pac4j.sparkjava.CallbackRoute;
+import org.pac4j.oauth.credentials.OAuth20Credentials;
 import org.pac4j.sparkjava.DefaultHttpActionAdapter;
 import org.pac4j.sparkjava.SecurityFilter;
 import org.pac4j.sparkjava.SparkWebContext;
 
-import static spark.Spark.*;
+import static spark.Spark.before;
+import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
         Google2Client googleClient = new Google2Client("", "");
 
-        googleClient.setScope(Google2Client.Google2Scope.PROFILE);
+        googleClient.setScope(Google2Client.Google2Scope.EMAIL_AND_PROFILE);
 
         Clients client = new Clients("http://localhost:4567/callback", googleClient);
 
@@ -22,15 +21,16 @@ public class Main {
 
         config.setHttpActionAdapter(new DefaultHttpActionAdapter());
 
-        before("/login", new SecurityFilter(config, "Google2Client"));
+        before("/login", new SecurityFilter(config, "Google2Client", "domain"));
 
         get("/", (req, res) -> "hello world!");
 
-        Gson gson = new Gson();
+        get("/callback", ((request, response) -> {
+            SparkWebContext context = new SparkWebContext(request, response);
 
-//        CallbackRoute callback = new CallbackRoute(config, null, true);
-//
-        get("/callback", ((request, response) -> request.body()));
-//        post("/callback", callback);
+            OAuth20Credentials credentials = googleClient.getCredentials(context);
+
+            return googleClient.getUserProfile(credentials, context).getAttributes();
+        }));
     }
 }
